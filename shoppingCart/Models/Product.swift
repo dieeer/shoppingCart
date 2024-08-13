3769856c01c65430feae89abb29c721070d31202
justin reid <hi@justinre.id>
2024-08-13 19:13:15 +0100

    logic for shopping cart down

--- shoppingCart/Models/Product.swift
+++ shoppingCart/Models/Product.swift
@@ -5,6 +5,7 @@
 //  Created by Justin Reid on 13/08/2024.
 //
 
+import Foundation
 
 enum Category {
     case suspension
@@ -15,10 +16,19 @@ enum Category {
 
 class Product {
     
+    let id: String
     let category: Category
     let name: String
     let SKU: String
     let price: Decimal
+    
+    init(id: String, category: Category, name: String, SKU: String, price: Decimal) {
+        self.id = id
+        self.category = category
+        self.name = name
+        self.SKU = SKU
+        self.price = price
+    }
 }
 
 struct Promotion {
--- shoppingCart/Models/ShoppingCart.swift
+++ shoppingCart/Models/ShoppingCart.swift
@@ -8,11 +8,35 @@
 import Foundation
 
 protocol ShoppingCarting {
+    var products: [Product] { get set }
+    
     func add(item: Product)
-    func removeItem()
+    func removeItem(with id: String)
+    func clearCart()
 }
 
 class ShoppingCart: ShoppingCarting {
+
+    var discounter: Discounting?
     var products: [Product] = []
     
+    init(discounter: Discounting? = nil, products: [Product] = []) {
+        self.discounter = discounter
+    }
+    
+    var totalPrice: Decimal {
+        return products.reduce(0) { $0 + $1.price }
+    }
+    
+    func add(item: Product) {
+        products.append(item)
+    }
+    
+    func removeItem(with id: String) {
+        products.removeAll { $0.id == id }
+    }
+    
+    func clearCart() {
+        products = []
+    }
 }
