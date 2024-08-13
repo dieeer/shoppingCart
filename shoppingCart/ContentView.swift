a62844930be0670934ecfc21f7c6b7eecc7dacce
justin reid <hi@justinre.id>
2024-08-13 17:13:02 +0100

    Initial Commit

---
+++ shoppingCart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shoppingCart
+//
+//  Created by Justin Reid on 13/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shoppingCart/shoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingCartApp.swift
+//  shoppingCart
+//
+//  Created by Justin Reid on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct shoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
